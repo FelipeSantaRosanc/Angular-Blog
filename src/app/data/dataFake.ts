3a7d43299671a9887bcdb686ac514adc920ee459
Your Name <you@example.com>
2023-09-22 09:40:17 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Luffy é eleito melhor protagonista shounen",
+    "description": "O capitão dos Chapéu de palha foi anunciado hoje como principal protagonita shounen pela Toei.",
+    "photoCover": "https://criticalhits.com.br/wp-content/uploads/2022/08/luffy-one-piece-1015.jpg"
+    },
+    {
+      "id": "2",
+      "title": "Zoro e Sanji quebram recordes e ficam incríveis 3 horas sem brigar",
+      "description": " os 2 idiotas quebraram mais coisas dentro do Sunny!",
+      "photoCover": "https://staticg.sportskeeda.com/editor/2022/03/706ad-16474626086571-1920.jpg"
+      },
+      {
+        "id": "3",
+        "title": "Yamato se juntará em breve à tripulação?",
+        "description": "Com certeza uma das partes mais triste da despedida do bando de Wano, foi a não entrada (oficial) do Yamato no bando!",
+        "photoCover": "https://johto.legiaodosherois.com.br/wp-content/uploads/2022/09/legiao_SB9Ad654ziRq.jpg"
+        },
+        {
+          "id": "4",
+          "title": "Tripulação Yonkou tem sua primeira grande batalha.",
+          "description": "Após entrar em  Egghead, Borsalino e Saturn vão atrás dos Chapéu de palha!",
+          "photoCover": "https://sportshub.cbsistatic.com/i/2021/08/09/f9f487fc-3c32-40f6-ba90-dc1ce083c962/one-piece-anime-opening-update-new-straw-hats-jimbei-1274612.jpg"
+          },
+
+]
